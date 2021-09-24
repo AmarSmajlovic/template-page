2d6bdd0f653665a205d290ec14b37e8e5995663d
AmarSmajlovic <amarcsgo@gmail.com>
2021-09-25 00:45:55 +0200

    added logic for nav scroll

--- client/script.js
+++ client/script.js
@@ -1,1 +1,12 @@
-console.log('123');
+//Logic for nav scroll
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll',() => {
+    const offset = window.pageYOffset;
+    if(offset>75){
+        nav.classList.add('scroll');
+    }
+    else {
+        nav.classList.remove('scroll');
+    }
+})
